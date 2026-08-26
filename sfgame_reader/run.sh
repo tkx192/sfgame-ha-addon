62bf90ee55a4a6cda8d4a85eeb39a81c4000827f
T.K.X. <tkx192@yahoo.de>
2026-08-26 11:04:16 +0200

    Refactor run.sh to use bashio for configuration

--- sfgame_reader/run.sh
+++ sfgame_reader/run.sh
@@ -1,14 +1,29 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
+
 set -e
 
-echo "[INFO] Starting Shakes & Fidget Reader"
+bashio::log.info "Starting Shakes & Fidget Reader"
 
-for var in MQTT_HOST MQTT_PORT MQTT_USERNAME MQTT_PASSWORD; do
-    if [[ -n "${!var:-}" ]]; then
-        echo "[INFO] $var is present"
-    else
-        echo "[INFO] $var is missing"
-    fi
-done
+USERNAME="$(bashio::config 'sfgame_username')"
+PASSWORD="$(bashio::config 'sfgame_password')"
+CHARACTER="$(bashio::config 'character_name')"
+POLL_INTERVAL="$(bashio::config 'poll_interval_seconds')"
+PUBLISH_FULL="$(bashio::config 'publish_full_gamestate')"
+
+MQTT_HOST="$(bashio::services mqtt 'host')"
+MQTT_PORT="$(bashio::services mqtt 'port')"
+MQTT_USERNAME="$(bashio::services mqtt 'username')"
+MQTT_PASSWORD="$(bashio::services mqtt 'password')"
+
+export SFGAME_USERNAME="$USERNAME"
+export SFGAME_PASSWORD="$PASSWORD"
+export SFGAME_CHARACTER_NAME="$CHARACTER"
+export SFGAME_POLL_INTERVAL_SECONDS="$POLL_INTERVAL"
+export SFGAME_PUBLISH_FULL_GAMESTATE="$PUBLISH_FULL"
+
+export MQTT_HOST
+export MQTT_PORT
+export MQTT_USERNAME
+export MQTT_PASSWORD
 
 exec /usr/bin/sfgame-reader
