b698c07617f968ce9ffe606e1bba442343d67dd2
T.K.X. <tkx192@yahoo.de>
2026-08-26 11:01:56 +0200

    Check for presence of MQTT environment variables
    Added checks for required environment variables in run.sh.

--- sfgame_reader/run.sh
+++ sfgame_reader/run.sh
@@ -3,4 +3,12 @@ set -e
 
 echo "[INFO] Starting Shakes & Fidget Reader"
 
+for var in MQTT_HOST MQTT_PORT MQTT_USERNAME MQTT_PASSWORD; do
+    if [[ -n "${!var:-}" ]]; then
+        echo "[INFO] $var is present"
+    else
+        echo "[INFO] $var is missing"
+    fi
+done
+
 exec /usr/bin/sfgame-reader
