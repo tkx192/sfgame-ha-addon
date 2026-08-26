89ec3aa852e5c2883611331d69828d2f9acf3ffb
T.K.X. <tkx192@yahoo.de>
2026-08-26 10:23:16 +0200

    Update run.sh

--- sfgame_reader/run.sh
+++ sfgame_reader/run.sh
@@ -10,6 +10,16 @@ export CHARACTER_NAME="$(jq -r '.character_name // empty' /data/options.json)"
 export POLL_INTERVAL_SECONDS="$(jq -r '.poll_interval_seconds // 300' /data/options.json)"
 export PUBLISH_FULL_GAMESTATE="$(jq -r '.publish_full_gamestate // false' /data/options.json)"
 
+MQTT_HOST="$(bashio::services mqtt host)"
+MQTT_PORT="$(bashio::services mqtt port)"
+MQTT_USERNAME="$(bashio::services mqtt username)"
+MQTT_PASSWORD="$(bashio::services mqtt password)"
+
+export MQTT_HOST
+export MQTT_PORT
+export MQTT_USERNAME
+export MQTT_PASSWORD
+
 echo "[INFO] Configuration loaded"
 
 exec /usr/bin/sfgame-reader
